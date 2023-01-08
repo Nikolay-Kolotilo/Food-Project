112486e74dadb70687f0c5cae47412d237e6cba4
Nikolay_Kolotilo <3dnikik00@gmail.com>
2023-01-08 21:40:49 +0200

    Practice according to the course of lecture 94

--- js/script.js
+++ js/script.js
@@ -383,6 +383,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //Lec_92 Организация Слайдера. See lec 92 by myself.
     //Lec_93. Carousel (Взамен лекции 92.). Вариант слайдера в виде карусели.
     const slides = document.querySelectorAll(".offer__slide"),
+        slider = document.querySelector('.offer__slider'), //Lec_94. 2:00 Создаем контейнер для слайдера.
         prev = document.querySelector(".offer__slider-prev"),
         next = document.querySelector(".offer__slider-next"),
         total = document.querySelector('#total'),
@@ -413,6 +414,54 @@ window.addEventListener('DOMContentLoaded', () => {
     slides.forEach(slide => {
         slide.style.width = width;
     });
+    
+    slider.style.position = 'relative'; //Lec_94. 2:30 Позиция размещения - Релатив.
+
+    const indicatorsDots = document.createElement('ol'),
+          dots = []; //Lec_94. 9:00 Создание массива для всех созданных точек.
+    indicatorsDots.classList.add('carousel-indicators');
+    //Lec_94. 3:45 Стилизация блока карусели:
+    indicatorsDots.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+    `; 
+    slider.append(indicatorsDots);
+
+     //Lec_94. 3:45 Создание слайдов по количеству точек.
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+            `;
+             //Lec_94. 8:00 Назначение класса активности.
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicatorsDots.append(dot);     
+        dots.push(dot);
+    };
+
 
     next.addEventListener('click', () => {
         if (ofset == +width.slice(0, width.length - 2)*(slides.length - 1) ) { //Превращение  width (650px) в 650.
@@ -432,6 +481,8 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         } 
+        dots.forEach(dot => dot.style.opacity = '.5'); //Lec_94. 10:00 Назначение класса активности событию.
+        dots[slideIndex - 1].style.opacity = '1';
     });
 
     prev.addEventListener('click', () => {
@@ -452,6 +503,30 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         } 
+        dots.forEach(dot => dot.style.opacity = '.5'); //Lec_94. 10:55 Назначение класса активности событию.
+        dots[slideIndex - 1].style.opacity = '1';
     });
+
+    //Lec_94. 11:20. Добавление события на точки.
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');// Lec_94. 11:50. Получение значения атрибута.
+
+            slideIndex = slideTo;// Lec_94. 12:30. Присваивание индексу значения атрибута полученного ранее.
+            ofset = +width.slice(0, width.length - 2) * (slideTo - 1); //Lec_94. 12:50. Передаижение по галлерее.
+            slidesField.style.transform = `translateX(-${ofset}px)`;
+            
+            if (slides.length < 10 ) {//Lec_94. 13:50. Применение счетчика на точках.
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            } 
+            dots.forEach(dot => dot.style.opacity = '.5'); //Lec_94. 13:40. Изменение отображения точек.
+            dots[slideIndex - 1].style.opacity = '1';
+            
+
+        });
+    });
+
 });
 
