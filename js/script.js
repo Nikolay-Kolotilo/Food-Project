09958c1f358ec7470af5bb7c35585e41ebe043a8
Nikolay_Kolotilo <3dnikik00@gmail.com>
2023-01-17 03:04:35 +0200

    Practice according to the course of lecture 98

--- js/script.js
+++ js/script.js
@@ -658,24 +658,3 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-    const p1 = new Promise((resolve, reject) => {
-      setTimeout(resolve, 1000, "one");
-    });
-    const p2 = new Promise((resolve, reject) => {
-      setTimeout(resolve, 2000, "two");
-    });
-    const p3 = new Promise((resolve, reject) => {
-      setTimeout(resolve, 3000, "three");
-    });
-    const p4 = new Promise((resolve, reject) => {
-      setTimeout(resolve, 4000, "four");
-    });
-    const p5 = new Promise((resolve, reject) => {
-      reject("reject");
-    });
-     
-    Promise.all([p1, p2, p3, p4, p5]).then(value => {
-      console.log(value);
-    }, reason => {
-      console.log(reason)
-    });
